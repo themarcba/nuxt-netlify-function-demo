84507818399c160dacb299f798e2c76c01a204bd
Marc Backes <hello@marc.dev>
2021-05-21 20:37:50 +0200

    tweaked the function

--- functions/hello.js
+++ functions/hello.js
@@ -1,4 +1,28 @@
 const uniqid = require('uniqid')
+const greetings = [
+  'hello',
+  'hi',
+  'salutations',
+  'greetings',
+  'welcome',
+  'ahoy',
+  'ello there, mate',
+  'hola',
+  'yo',
+]
+
+const getRandomGreeting = () => {
+  const n = Math.floor(Math.random() * greetings.length)
+  return greetings[n]
+}
+
 exports.handler = async (event, context) => {
-    return { statusCode: 200, body: JSON.stringify({ message: 'hello', uid: uniqid(), at: new Date() }) };
-};
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      at: new Date(),
+      uid: uniqid(),
+      greeting: getRandomGreeting(),
+    }),
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="container">
-    <div class="m-4">
-      <h1 class="title">Netlify Functions Test</h1>
-      <button
-        class="
-          py-2
-          px-4
-          font-bold
-          text-white
-          bg-indigo-500
-          hover:bg-indigo-700
-          rounded-md
-          transition-color
-          duration-300
-        "
-        @click="fetchData"
-      >
-        Load data
-      </button>
-      <pre v-if="data">{{ data }}</pre>
-    </div>
+  <div class="m-4">
+    <h1 class="title text-2xl font-bold">Netlify Functions Test</h1>
+    <button
+      class="
+        my-4
+        py-2
+        px-4
+        font-bold
+        text-white
+        bg-indigo-500
+        hover:bg-indigo-700
+        rounded-md
+        transition-color
+        duration-300
+      "
+      @click="fetchData"
+    >
+      Load data
+    </button>
+    <pre v-if="hasData && !isLoading">{{ data }}</pre>
+    <div v-else-if="isLoading">Hold on a second...</div>
   </div>
 </template>
 
@@ -28,11 +28,19 @@ export default {
   data() {
     return {
       data: {},
+      isLoading: false,
     }
+  },
+  computed: {
+    hasData() {
+      return Object.keys(this.data).length > 0
+    },
   },
   methods: {
     async fetchData() {
+      this.isLoading = true
       const response = await this.$axios.get('/api/hello')
+      this.isLoading = false
       this.data = response.data
     },
   },
