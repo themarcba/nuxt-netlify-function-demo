92c8968ac9ec0e91c77e27c06a9c67e998754386
Marc Backes <hello@marc.dev>
2021-05-22 13:59:43 +0200

    removed some unnecessary code

--- pages/claps.vue
+++ pages/claps.vue
@@ -4,18 +4,3 @@
     <clap-button id="demo-clap-button" />
   </div>
 </template>
-
-<script>
-export default {
-  methods: {
-    async createClap() {
-      const response = await this.$axios.post(`/api/claps`, {
-        id: Math.floor(Math.random() * 99999999).toString(),
-      })
-      this.claps = response.data.claps
-    },
-  },
-}
-</script>
-
-<style></style>
