de3f8a5747da66d8fc0a0c23bb94274e5dae8b41
Marc Backes <hello@marc.dev>
2021-05-22 14:02:56 +0200

    removed dedicated claps view

--- pages/claps.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="claps m-4">
-    <h1 class="text-3xl font-bold text-green-500">Serverless Clap Button</h1>
-    <clap-button id="demo-clap-button" />
-  </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,6 @@
 <template>
-  <div class="m-4">
-    <h1 class="title text-2xl font-bold">Netlify Functions Test</h1>
-    <button
-      class="
-        my-4
-        py-2
-        px-4
-        font-bold
-        text-white
-        bg-indigo-500
-        hover:bg-indigo-700
-        rounded-md
-        transition-color
-        duration-300
-      "
-      @click="fetchData"
-    >
-      <span v-if="claps">{{ claps }} Clap!</span>
-      <span v-else>Clap!</span>
-    </button>
+  <div class="claps m-4">
+    <h1 class="text-3xl font-bold text-green-500">Serverless Clap Button</h1>
+    <clap-button id="demo-clap-button" />
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      data: {},
-      claps: 0,
-    }
-  },
-  computed: {
-    hasData() {
-      return Object.keys(this.data).length > 0
-    },
-  },
-  methods: {
-    async fetchData() {
-      this.isLoading = true
-      const response = await this.$axios.get('/api/hello')
-      this.isLoading = false
-      this.data = response.data
-    },
-  },
-}
-</script>
-
-<style></style>
