fa663b8bda6ebfb52940c991b755761b456d6965
Marc Backes <hello@marc.dev>
2021-05-22 13:59:07 +0200

    added clap buffer to the button

--- pages/claps.vue
+++ pages/claps.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="claps m-4">
     <h1 class="text-3xl font-bold text-green-500">Serverless Clap Button</h1>
-    <clap-button id="test-clap-button7" />
+    <clap-button id="demo-clap-button" />
   </div>
 </template>
 
